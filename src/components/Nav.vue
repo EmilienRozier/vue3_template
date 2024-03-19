5f0ae94f4355091e4bfb7a690ad76056aaaacef5
EmilienRozier <emilienrozier@icloud.com>
2024-03-19 12:08:34 +0100

    menu

---
+++ src/components/Nav.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav>
+    <ul>
+      <li><a href="/">menu</a></li>
+      <li><a href="#">menu</a></li>
+      <li><a href="#">menu</a></li>
+      <li><a href="#">menu</a></li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  width: 100%;
+  margin: 25px 0;
+}
+
+nav>ul {
+  display: flex;
+  justify-content: space-around;
+  width: 50%;
+  margin: 0 auto;
+}
+
+nav>ul>li {
+  list-style: none;
+}
+</style>
